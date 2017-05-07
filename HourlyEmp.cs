9df77404b358701770aedb6f1072eb38c1fa8efb
damonwalker <damon.walker520@gmail.com>
2017-05-07 12:09:45 -0500

    Update HourlyEmp.cs

--- HourlyEmp.cs
+++ HourlyEmp.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,20 +8,11 @@ namespace RunEmployee
 {
     public class HourlyEmp : Employee
     {
-        //private float rate; // wage per hour
-        //private float hours;
-
-
-        //public HourlyEmp( string first, string last, string ssn, decimal hourlyWage, decimal hoursWorked): base(first, last, ssn)
-        //public HourlyEmp()
-        //{
-        //    rate = hourlyWage;
-        //}
         
-       
-
         public override void ComputeGross()
         {
+            Console.WriteLine("Please enter the Employee's Name: ");
+            name = Console.ReadLine();
             Console.WriteLine("Please enter the amount of hours worked this week: ");
             Hours = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter the rate of pay: ");
@@ -35,6 +26,9 @@ namespace RunEmployee
                     gross = (40 * Rate) + ((Hours - 40) * (Rate * 1.5f));
             }
         }
+
+        public string Name { get; set; }
+
         public float Rate
         {
             get
@@ -64,34 +58,7 @@ namespace RunEmployee
                     throw new ArgumentOutOfRangeException("Hours", value, "Rate must be >=0 and <=168");
             }// end set
         }//end property Rate
-         //public float ComputePay // may change to compute gross
-         //{
-         //    get
-         //    {
-         //    return pay;
-         //    {   // end get          
-
-        //    set
-
-        //    {
-        //    //    if (value <= 40)
-        //    //                pay = value;
-        //    //    else
-        //    //                pay = (Hours - 40) * Rate * 1.5;
-
-
-
-        //    } // end set
-        //        //return base.ComputeGross() + ((Hours - 40) * Rate * 1.5M);
-        //        //Console.WriteLine("Pay is {0}, \n,", pay);
-        //        //DisplayMenu();
-
-        
-        public override string ToString()
-        {
-            return string.Format("Pay is equal to: ", gross);
-        }
-
+         
 
 
 
